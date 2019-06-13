df38defeed739428218ba1ef533d61421354a000
kleint <kleint@semantic-web.at>
2019-06-13 09:00:37 +0200

    init

---
+++ index.ts
@@ -0,0 +1,19 @@
+import * as express from "express";
+
+const app = express();
+const port = 3000;
+
+const concepts = [
+    {
+        label: "label 1",
+        children: 3
+    },
+    {
+        label: "label 2",
+        children: 0
+    }
+]
+
+app.get("/api/results", (req, res) => res.json(concepts));
+
+app.listen(port, () => console.log(`Server running on port ${ port }`));
